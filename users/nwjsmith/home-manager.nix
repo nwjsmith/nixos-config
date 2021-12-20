fcb4732acbc2a371894ed6bb52589a23d85f77ff
Nate Smith <nate@theinternate.com>
2021-12-20 12:56:50 -0500

    Config fixups

--- users/nwjsmith/home-manager.nix
+++ users/nwjsmith/home-manager.nix
@@ -59,27 +59,15 @@
     userEmail = "nate@theinternate.com";
     aliases = {
       st = "status";
+      dc = "diff --cached";
+      di = "diff";
+    };
+    extraConfig = {
+      core.askpass = "";
     };
     signing = {
       key = "F2089547768B6CCA";
       signByDefault = true;
-    };
-  };
-
-  programs.alacritty = {
-    enable = true;
-
-    settings = {
-      env.TERM = "xterm-256color";
-
-      key_bindings = [
-      { key = "K"; mods = "Command"; chars = "ClearHistory"; }
-      { key = "V"; mods = "Command"; action = "Paste"; }
-      { key = "C"; mods = "Command"; action = "Copy"; }
-      { key = "Key0"; mods = "Command"; action = "ResetFontSize"; }
-      { key = "Equals"; mods = "Command"; action = "IncreaseFontSize"; }
-      { key = "Subtract"; mods = "Command"; action = "DecreaseFontSize"; }
-      ];
     };
   };
 
