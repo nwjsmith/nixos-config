c59b2aa7bd4ac1b1ac35b7b22fff692336591068
Nate Smith <nate@theinternate.com>
2021-12-22 13:53:19 -0500

    More tweaks

--- users/nwjsmith/home-manager.nix
+++ users/nwjsmith/home-manager.nix
@@ -9,7 +9,6 @@
 
   home.packages = with pkgs; [
     bat
-    fzf
     htop
     jq
     nodejs
@@ -25,12 +24,22 @@
     LANG = "en_CA.UTF-8";
     LC_CTYPE = "en_CA.UTF-8";
     LC_ALL = "en_CA.UTF-8";
+    EDITOR = "nvim";
   };
 
   home.file.".inputrc".source = ./inputrc;
 
   xdg.configFile."i3/config".text = builtins.readFile ./i3;
-  xdg.configFile."rofi/config.rasi".text = builtins.readFile ./rofi;
+
+  programs.rofi = {
+    enable = true;
+    theme = "gruvbox-light";
+    font = "Fira Code 13";
+    terminal = "${pkgs.kitty}/bin/kitty";
+    extraConfig = {
+      dpi = 192;
+    };
+  };
 
   #---------------------------------------------------------------------
   # Programs
@@ -57,6 +66,9 @@
 
   programs.zsh.enable = true;
   programs.starship.enable = true;
+  programs.fzf = {
+    enable = true;
+  };
 
   programs.git = {
     enable = true;
@@ -85,10 +97,11 @@
   programs.neovim = {
     enable = true;
     plugins = with pkgs.vimPlugins; [
-      gruvbox-material
-      (nvim-treesitter.withPlugins (plugins: pkgs.tree-sitter.allGrammars))
+      fzf-vim
       gitsigns-nvim
+      gruvbox-community
       nvim-lspconfig
+      (nvim-treesitter.withPlugins (plugins: pkgs.tree-sitter.allGrammars))
       vim-commentary
       vim-eunuch
       vim-fugitive
@@ -98,19 +111,63 @@
       vim-speeddating
       vim-surround
       vim-test
-      telescope-fzy-native-nvim
       vim-vinegar
     ];
     extraConfig = ''
       set termguicolors
       set background=light
-      let g:gruvbox_material_palette="original"
-      colorscheme gruvbox-material
+      colorscheme gruvbox
     '';
   };
 
   programs.i3status = {
     enable = true;
+    enableDefault = false;
+    general = {
+      colors = true;
+      color_good = "#98971a";
+      color_bad = "#cc241d";
+      color_degraded = "#d79921";
+      color_separator = "#7c6f64";
+      interval = 1;
+    };
+    modules = {
+      "ethernet _first_" = {
+        position = 1;
+        settings = {
+          format_down = "E: down";
+          format_up = "%ip";
+        };
+      };
+      "disk /" = {
+        position = 2;
+        settings = {
+          format = "%avail";
+        };
+      };
+      memory = {
+        position = 3;
+        settings = {
+          format = "%used / %available";
+          format_degraded = "MEMORY < %available";
+          threshold_degraded = "4G";
+        };
+      };
+      "tztime local" = {
+        position = 4;
+        settings = {
+          format = "%y-%m-%d %H:%M";
+          separator = false;
+        };
+      };
+      "tztime utc" = {
+        position = 5;
+        settings = {
+          format = "(%H:%M)";
+          timezone = "UTC";
+        };
+      };
+    };
   };
 
   services.gpg-agent = {
